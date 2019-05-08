8a642d7423cfcfc0853493823fc823851a79c14f
Lyaaaaaaaaaaaaaaa <44974422+Lyaaaaaaaaaaaaaaa@users.noreply.github.com>
2019-05-08 09:37:24 +0200

    Updated Main
    No longer need to call read_line. set_filters will call it by itself.

--- src/log_filter_main.adb
+++ src/log_filter_main.adb
@@ -1,7 +1,11 @@
 with Log_Filter; use Log_Filter;
+with Text_IO; use Text_IO;
 procedure Log_Filter_Main is
+
 begin
-   select_file("./18th L.txt"); --shall later make it dynamic
-   read_line;
+
+
+   --select_file("./18th L.txt"); --shall later make it dynamic (with the interface)
+   set_filters;
    null;
 end Log_Filter_Main;
